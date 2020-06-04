695f8d2c47612fdbc1b9a6dc93660cacbbdb86bd
thanhlongdev <vothanhlong741@gmail.com>
2020-06-04 22:11:56 +0700

    Add object helper

--- src/main/java/vn/team/system/App.java
+++ src/main/java/vn/team/system/App.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import vn.team.system.common.ObjectHelper;
+import vn.team.system.common.info.UserInfo;
 
 @SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class App {
